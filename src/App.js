abc5322a19b23a36da61e7d9bf066c7d1668fafc
Marek Zelinka <21342072+marekzelinka@users.noreply.github.com>
2021-05-13 18:29:02 +0200

    complex state

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
 import { useState } from 'react'
 
-const Display = ({ counter }) => <div>{counter}</div>
+const History = ({ allClicks }) =>
+  allClicks.length === 0 ? (
+    <div>the app is used by pressing the buttons</div>
+  ) : (
+    <div>button press history: {allClicks.join(' ')}</div>
+  )
 
 const Button = ({ text, ...restProps }) => (
   <button {...restProps}>{text}</button>
 )
 
 const App = () => {
-  const [counter, setCounter] = useState(0)
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAllClicks] = useState([])
 
-  const increment = () => setCounter((counter) => counter + 1)
+  const handleLeftClick = () => {
+    setLeft((left) => left + 1)
+    setAllClicks((allClicks) => allClicks.concat('L'))
+  }
 
-  const setToZero = () => setCounter(0)
+  const handleRightClick = () => {
+    setRight((right) => right + 1)
+    setAllClicks((allClicks) => allClicks.concat('R'))
+  }
+
   return (
     <div>
-      <Display counter={counter} />
-      <Button onClick={increment} text="plus" />
-      <Button onClick={setToZero} text="zero" />
+      {left}
+      <Button onClick={handleLeftClick} text="left" />
+      <Button onClick={handleRightClick} text="right" />
+      {right}
+      <History allClicks={allClicks} />
     </div>
   )
 }
