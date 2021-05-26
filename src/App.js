a935b9e33b1645d1ed3715676a816d2b9086430f
Marek Zelinka <21342072+marekzelinka@users.noreply.github.com>
2021-05-26 17:30:46 +0200

    Revert "complex state"
    This reverts commit abc5322a19b23a36da61e7d9bf066c7d1668fafc.

--- src/App.js
+++ src/App.js
@@ -1,38 +1,22 @@
 import { useState } from 'react'
 
-const History = ({ allClicks }) =>
-  allClicks.length === 0 ? (
-    <div>the app is used by pressing the buttons</div>
-  ) : (
-    <div>button press history: {allClicks.join(' ')}</div>
-  )
+const Display = ({ counter }) => <div>{counter}</div>
 
 const Button = ({ text, ...restProps }) => (
   <button {...restProps}>{text}</button>
 )
 
 const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAllClicks] = useState([])
+  const [counter, setCounter] = useState(0)
 
-  const handleLeftClick = () => {
-    setLeft((left) => left + 1)
-    setAllClicks((allClicks) => allClicks.concat('L'))
-  }
+  const increment = () => setCounter((counter) => counter + 1)
 
-  const handleRightClick = () => {
-    setRight((right) => right + 1)
-    setAllClicks((allClicks) => allClicks.concat('R'))
-  }
-
+  const setToZero = () => setCounter(0)
   return (
     <div>
-      {left}
-      <Button onClick={handleLeftClick} text="left" />
-      <Button onClick={handleRightClick} text="right" />
-      {right}
-      <History allClicks={allClicks} />
+      <Display counter={counter} />
+      <Button onClick={increment} text="plus" />
+      <Button onClick={setToZero} text="zero" />
     </div>
   )
 }
